005dcd73e5c2ecea80964de84ed0466464b5676f
Robert Sandor <sandorrobertk94@gmail.com>
2015-05-14 23:36:17 +0300

    Ingredient updated with operators and getters/setters

--- oop_lab11/Ingredient.cpp
+++ oop_lab11/Ingredient.cpp
@@ -3,3 +3,19 @@
 //
 
 #include "Ingredient.h"
+
+bool Ingredient::operator==(const Ingredient &rhs, const Ingredient &lhs) const {
+    return rhs._id == lhs._id;
+}
+
+bool Ingredient::operator!=(const Ingredient &rhs, const Ingredient &lhs) const {
+    return rhs._id != lhs._id;
+}
+
+Ingredient &Ingredient::operator=(Ingredient other) {
+    _id = other._id;
+    _name = other._name;
+    _provider = other._provider;
+    _quantity = other._quantity;
+    return *this;
+}
--- oop_lab11/Ingredient.h
+++ oop_lab11/Ingredient.h
@@ -5,9 +5,58 @@
 #ifndef OOP_LAB11_INGREDIENT_H
 #define OOP_LAB11_INGREDIENT_H
 
+#include "string"
 
 class Ingredient {
+public:
+    Ingredient(unsigned int _id, const std::string &_name, const std::string &_provider, unsigned int _quantity) : _id(
+            _id), _name(_name), _provider(_provider), _quantity(_quantity) { }
 
+    virtual ~Ingredient() { }
+
+    unsigned int get_id() const {
+        return _id;
+    }
+
+    const std::string &get_name() const {
+        return _name;
+    }
+
+    const std::string &get_provider() const {
+        return _provider;
+    }
+
+    unsigned int get_quantity() const {
+        return _quantity;
+    }
+
+    void set_name(const std::string &_name) {
+        Ingredient::_name = _name;
+    }
+
+    void set_provider(const std::string &_provider) {
+        Ingredient::_provider = _provider;
+    }
+
+    void set_quantity(unsigned int _quantity) {
+        Ingredient::_quantity = _quantity;
+    }
+
+    const std::string &toSTDString() const {
+        return std::to_string(_id) + " : " + _name + " : " + _provider + " : " + std::to_string(_quantity);
+    }
+
+    inline bool operator==(const Ingredient &rhs, const Ingredient &lhs) const;
+
+    inline bool operator!=(const Ingredient &rhs, const Ingredient &lhs) const;
+
+    Ingredient &operator=(Ingredient other);
+
+private:
+    unsigned int _id;
+    std::string _name;
+    std::string _provider;
+    unsigned int _quantity;
 };
 
 
