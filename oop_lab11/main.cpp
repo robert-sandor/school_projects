94a4fae4f3db1b9047910a8c0a74f0a34d7ce765
Robert Sandor <sandorrobertk94@gmail.com>
2015-05-19 00:31:30 +0300

    Readded MainWindow class

--- oop_lab11/main.cpp
+++ oop_lab11/main.cpp
@@ -1,12 +1,9 @@
 #include "QtWidgets"
-#include "MainWindow.h"
 #include <iostream>
 
 using namespace std;
 
 int main ( int argc, char ** argv ) {
     QApplication app ( argc, argv );
-    MainWindow main_win;
-    main_win.show ();
     return app.exec ();
 }
