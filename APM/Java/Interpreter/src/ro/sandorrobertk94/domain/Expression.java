2cd667df0f82a8707bd19822880e1f180d738aa5
robert-sandor <sandorrobertk94@gmail.com>
2015-10-11 11:29:12 +0300

    Added Interpreter project in Java

---
+++ APM/Java/Interpreter/src/ro/sandorrobertk94/domain/Expression.java
@@ -0,0 +1,8 @@
+package ro.sandorrobertk94.domain;
+
+/**
+ * Created by Robert on 10/11/2015.
+ */
+public abstract class Expression {
+    public abstract String toString();
+}
---
+++ APM/Java/Interpreter/src/ro/sandorrobertk94/domain/IStatement.java
@@ -0,0 +1,8 @@
+package ro.sandorrobertk94.domain;
+
+/**
+ * Created by Robert on 10/11/2015.
+ */
+public interface IStatement {
+    String toString();
+}
